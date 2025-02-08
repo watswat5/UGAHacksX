63e45a6713ad57772be06273c622c96b79b09f26
km49142 <167227544+km49142@users.noreply.github.com>
2025-02-08 00:34:10 -0500

    First upload

---
+++ wrover_test_1.ino
@@ -0,0 +1,27 @@
+#include <FixMath.h>
+#include <mozzi>
+
+#define THUMB_BUTTON_RIGHT
+#define THUMB_BUTTON_LEFT
+
+#define RIGHT_INDEX
+#define RIGHT_MIDDLE
+#define RIGHT_RING
+#define RIGHT_PINKEY
+
+#define LEFT_INDEX
+#define LEFT_MIDDLE
+#define LEFT_RING
+#define LEFT_PINKEY
+
+//speaker outputs to 25, 26
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
